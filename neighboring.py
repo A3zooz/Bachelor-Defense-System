cfe10368ae8889e848547be9250d2b8f1d2d0bad
A3zooz <hmoda201@gmail.com>
2022-08-20 01:53:08 +0200

    changes

--- neighboring.py
+++ neighboring.py
@@ -13,17 +13,18 @@ def neighbor(solution):
         
         if len(solution[1][solution[0][i]['Examiner']][solution[0][i]['Time']]) > 1:
             candidates.append(i)
-            print(solution[0][i])
+            # print(solution[0][i])
 
         
         
-        # if solution[2][solution[0]['Supervisor'][i]][solution[0]['Time'][i]] > 1:
-        #     candidates.append(i)
-        #     #print(solution[0][i])
+        if solution[2][solution[0]['Supervisor'][i]][solution[0]['Time'][i]] > 1:
+            candidates.append(i)
+            #print(solution[0][i])
 
     
-        # if solution[3][solution[0]['Room'][i]][solution[0]['Time'][i]] > 1:
-        #     candidates.append(i)
-        #     #print(solution[0][i])
+        if solution[3][solution[0]['Room'][i]][solution[0]['Time'][i]] > 1:
+            candidates.append(i)
+            #print(solution[0][i])
 
     print(set(candidates))
+    
