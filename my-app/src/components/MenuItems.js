b20629aeeac5a92f476c0165f01af32640ed775c
Terror-1 <elfarhosam487@gmail.com>
2022-09-02 11:58:38 +0200

    Navbar created

---
+++ my-app/src/components/MenuItems.js
@@ -0,0 +1,10 @@
+export const menuItems =[
+    {title : 'Home',
+     url :'/',
+     cName : 'nav-links'
+    },
+    {title : 'AboutUs',
+     url :'http://www.facebook.com/',
+     cName : 'nav-links'
+    }
+]
