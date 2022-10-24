6eb83c4ec172f59e9da63922b95242cef4901e04
anaselnemr <anasnemr25@gmail.com>
2022-10-24 20:53:03 +0200

    Done but needs testing

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -13,6 +13,7 @@ function Card() {
   const [dates,setDates]=useState([]);
   const [selectedslots,setSelectedslots]=useState(Array(dates.length*15).fill(0));
   const slotOptions = [
+    { label: "Select All", value: "all" },
     { value: 0, label: '1st' },
     { value: 1, label: '2nd' },
     { value: 2, label: '3rd' },
@@ -27,109 +28,132 @@ function Card() {
     { value: 11, label: '12th' },
     { value: 12, label: '13th' },
     { value: 13, label: '14th' },
-    { value: 14, label: '15th' },
-  ]
-  const loadExaminars = (searchExaminar) => {
-    return axios.get(url).then((res) => {
-      let list = [];
-      console.log(res.data);
-      res.data["externals"].forEach((ex) => list.push({ value: ex, label: ex }));
-      return list.filter((d) => d.label.toLowerCase().includes(searchExaminar.toLowerCase()));
-    });
-  };
-
-  const loadDates = (searchDate) => {
-    return axios.get(url).then((res) => {
-      const exams = res.data["dates"];
-      setDates(exams);
-      let list = [];
-      exams.forEach((ex) => list.push({ value: ex, label: ex }));
-      return list.filter((d) => d.label.toLowerCase().includes(searchDate.toLowerCase()));
-    });
-  };
-
-  const changeExaminar = (value) => {
-    setSelectedExaminar(value);
-    setSelectedslots(Array(dates.length*15).fill(0));
-    setDay([])
+    { value: 14, label: '15th' },]
+    const loadExaminars = (searchExaminar) => {
+      return axios.get(url).then((res) => {
+        let list = [];
+        console.log(res.data);
+        res.data["externals"].forEach((ex) => list.push({ value: ex, label: ex }));
+        return list.filter((d) => d.label.toLowerCase().includes(searchExaminar.toLowerCase()));
+      });
+    };
+  
+    const loadDates = (searchDate) => {
+      return axios.get(url).then((res) => {
+        const exams = res.data["dates"];
+        setDates(exams);
+        let list = [];
+        exams.forEach((ex) => list.push({ value: ex, label: ex }));
+        return list.filter((d) => d.label.toLowerCase().includes(searchDate.toLowerCase()));
+      });
+    };
+  
+    const changeExaminar = (value) => {
+      setSelectedExaminar(value);
+      setSelectedslots(Array(dates.length*15).fill(0));
+      setDay([])
+    }
+  
+    const changeDate = (value) => {
+      const tmp=dates.indexOf(SelectedDate.value)*15
+      let tmplist = [...selectedslots];
+      day.forEach((s) =>tmplist[tmp+s.value]=1)
+      setSelectedslots(tmplist);
+      const tmp2=dates.indexOf(value.value)*15
+      console.log(tmp2)
+      setSelectedDate(value)
+      tmplist=selectedslots.slice(tmp2, tmp2+15)
+      let tmplist2=[]
+      for(let i in tmplist){
+        if(tmplist[i]){
+          if(i==0){
+            tmplist2.push({ value: 0, label: '1st' })
+          }
+          else if(i==1){
+            tmplist2.push({ value: 1, label: '2nd' })  
+          }
+          else if(i==2){
+            tmplist2.push({ value: 2, label: '3rd' })  
+          }
+          else{
+            let tmp10=parseInt(i)+1
+            tmplist2.push({ value: parseInt(i), label: tmp10+'th' })    
+          }
+        }
+      }
+  
+      setDay(tmplist2)
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const tmp=dates.indexOf(SelectedDate.value)*15
+      let tmplist = [...selectedslots];
+  
+      day.forEach((s) =>tmplist[tmp+s.value]=1)
+      setSelectedslots(tmplist);
+  
+      let res={};
+      res[SelectedExaminar.value]=tmplist;
+      console.log("Done with temp")
+      console.log(tmplist);
+      axios.post('http://localhost:5000/external/',res)
+    }
+  
+    return (
+      <>
+        <form className={"Constarints-1"}>
+          <div className="divTitle">Examiner's Constraints</div>
+  
+  
+          <label className="divLabel-1">
+            Select Examiner <span className="required">*</span>
+          </label>
+  
+          <label className="divLabel-2">
+            Choose unavailable day <span className="required">*</span>
+          </label>
+          <label className="divLabel-3">
+            Choose unavailable slot <span className="required">*</span>
+          </label>
+          <AsyncSelect
+            className="dropdown-1"
+            cacheOptions
+            defaultOptions
+            value={SelectedExaminar}
+            getOptionLabel={(e) => e.label}
+            getOptionValue={(e) => e.value}
+            loadOptions={loadExaminars}
+            onChange={changeExaminar}
+          />
+          <AsyncSelect 
+            className="dropdown-2" 
+            cacheOptions
+            defaultOptions
+            value={SelectedDate}
+            getOptionLabel={(e) => e.label}
+            getOptionValue={(e) => e.value}
+            loadOptions={loadDates}
+            onChange={changeDate}
+          />
+          <Select  
+            className="dropdown-3" 
+            components={animatedComponents}
+            isMulti
+            value={day}
+            onChange={selected => {
+            selected.find(option => option.value === "all")
+              ? setDay(slotOptions.slice(1))
+              :setDay(selected)
+          }}
+            options={slotOptions}
+      //       autosize={true}
+      //       menuPlacement="auto"
+      // menuPosition="fixed"
+          />
+          <button className="btn-const" onClick={handleSubmit}>Add</button>
+        </form>
+      </>
+    );
   }
-
-  const changeDate = (value) => {
-    const tmp=dates.indexOf(SelectedDate.value)*15
-    let tmplist = [...selectedslots];
-    day.forEach((s) =>tmplist[tmp+s.value]=1)
-    setSelectedslots(tmplist);
-    setDay([])
-    setSelectedDate(value)  
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    const tmp=dates.indexOf(SelectedDate.value)*15
-    let tmplist = [...selectedslots];
-
-    day.forEach((s) =>tmplist[tmp+s.value]=1)
-    setSelectedslots(tmplist);
-
-    let res={};
-    res[SelectedExaminar.value]=tmplist;
-    console.log(tmplist);
-    console.log("Done with temp")
-    console.log(selectedslots);
-    axios.post('http://localhost:5000/external/',res)
-    setSelectedslots(Array(dates.length*15).fill(0));
-  }
-
-  return (
-    <>
-      <form className={"Constarints-1"}>
-        <div className="divTitle">Examiner's Constraints</div>
-
-
-        <label className="divLabel-1">
-          please select Examiner <span className="required">*</span>
-        </label>
-
-        <label className="divLabel-2">
-          Choose unavailable day <span className="required">*</span>
-        </label>
-        <label className="divLabel-3">
-          Choose unavailable slot <span className="required">*</span>
-        </label>
-        <AsyncSelect
-          className="dropdown-1"
-          cacheOptions
-          defaultOptions
-          value={SelectedExaminar}
-          getOptionLabel={(e) => e.label}
-          getOptionValue={(e) => e.value}
-          loadOptions={loadExaminars}
-          onChange={changeExaminar}
-        />
-        <AsyncSelect 
-          className="dropdown-2" 
-          cacheOptions
-          defaultOptions
-          value={SelectedDate}
-          getOptionLabel={(e) => e.label}
-          getOptionValue={(e) => e.value}
-          loadOptions={loadDates}
-          onChange={changeDate}
-        />
-        <Select  
-          className="dropdown-3" 
-          components={animatedComponents}
-          isMulti
-          value={day}
-          onChange={setDay}
-          options={slotOptions}
-    //       autosize={true}
-    //       menuPlacement="auto"
-    // menuPosition="fixed"
-        />
-        <button className="btn-const" onClick={handleSubmit}>Add</button>
-      </form>
-    </>
-  );
-}
-export default Card;
+  export default Card;
--- my-app/src/components/table.js
+++ my-app/src/components/table.js
@@ -1,46 +1,51 @@
-import React, { Component,useState,table, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import Timetable from 'react-scheduler-table'
-import { Link } from 'react-router-dom';
-const axios = require("axios").default;
-export default function Table(){
-    // const loadDates = (searchDate) => {
-    //     return axios.get(url).then((res) => {
-    //       const exams = res.data["dates"];
-    //       setDates(exams);
-    //       let list = [];
-    //       exams.forEach((ex) => list.push({ value: ex, label: ex }));
-    //       return list.filter((d) => d.label.toLowerCase().includes(searchDate.toLowerCase()));
-    //     });
-    //   };
+    import React, { Component,useState,table, useEffect } from 'react'
+    import LoadingScreen from "react-loading-screen";
+    import spinner from'./download.gif'
+    import ReactDOM from 'react-dom'
+    import Timetable from 'react-scheduler-table'
+    const axios = require("axios").default;
+    export default function Table(){
+        const [data, setData] = useState();
+        const [isLoding, setIsLoding] = useState(false);
+        const [showButton, setShowButton] = useState(true);
+        const [downButton, setdownButton] = useState(false);
+        const onGenerate=(event)=>{
+            setIsLoding(true)
+            setShowButton(false)
+            setdownButton(true)
+            return axios.post('http://localhost:5000/generate/').then((res) => {
+                console.log("HELLOOO")
+                console.log(res.data[0])
+                setData(res.data[0]);
+                console.log(data)
+                setIsLoding(false)
+                this.forceUpdate()
+                return table;
+        });
+        };
 
+        return (
+            <div> 
+            {isLoding ? (
+                <LoadingScreen
+            loading={true}
+            textColor="#676767"
+            logoSrc={spinner}
+            text="Loading..."
+            />
+            ) : (
+            <>
 
-    const [data, setData] = useState();
-    const [dwata, setDwata] = useState(false);
-    // useEffect(() => {
-    //     // Update the document title using the browser API
-    //     setDwata= true;
+            {showButton ? (<button  className="btn-const1" onClick={onGenerate}>Generate Solution</button>) :<> </> }
+            {downButton ?
+            (<form action='http://localhost:5000/downloadFile/'>
+            <input  className="btn-const1" type="submit" value="Download Solution" />
+            </form>) :
+            <></>}
+            </>
+            )}
 
-    //     });
-    const onGenerate=()=>{
-        return axios.post('http://localhost:5000/generate/').then((res) => {
-            console.log("HELLOOO")
-            setDwata = true;
-            this.forceUpdate()
-            return table;
-    });
-    };
+            </div> 
 
-
-
-
-    return (
-
-        <div> 
-        <button onClick={onGenerate}>Generate Solution</button>
-        <a href='http://localhost:5000/downloadFile/' download>Click to download</a>
-
-        </div> 
-
-    )
-}
+        )
+    }
