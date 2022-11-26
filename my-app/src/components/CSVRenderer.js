bbb654182082472769d53696336aac9b214b9e34
A3zooz <hmoda201@gmail.com>
2022-11-26 03:42:11 +0200

    deploy

--- my-app/src/components/CSVRenderer.js
+++ my-app/src/components/CSVRenderer.js
@@ -1,5 +1,5 @@
     import React from "react";
-    import  Table from "react-bootstrap";
+    import Table from 'react-bootstrap/Table';
 
     // 3. create customers table component
     const CSVRenderer = ({ data }) => {
