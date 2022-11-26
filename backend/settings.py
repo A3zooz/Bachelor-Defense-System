a03492dc6e8e1030e37ea5bb261490ca995dd10e
A3zooz <hmoda201@gmail.com>
2022-11-26 03:01:44 +0200

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'whiteNoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
